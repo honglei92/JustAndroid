f429239bd263d4df572bd2d3a3dd01d8a00ffe41
wanghonglei <wanghonglei@boco.com.cn>
2018-01-02 23:54:46 +0800

    retrofit杰哥rxjava

---
+++ app/src/main/java/com/boco/whl/funddemo/module/activity/annndroid/retrofit/WithRxJavatTest.java
@@ -0,0 +1,17 @@
+package com.boco.whl.funddemo.module.activity.annndroid.retrofit;
+
+import java.io.IOException;
+
+/**
+ * author:     wanghonglei@boco.com.cn
+ * desc:       描述-联合RxJava使用
+ * createTime: 2018/1/2 0002
+ * updateTime: 2018/1/2 0002
+ */
+
+public class WithRxJavatTest {
+
+    public static void main(String... args) throws IOException {
+
+    }
+}
