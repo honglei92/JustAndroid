6d3be834dc131e0c8dae80d046d17f54fe68d3ac
1667849141@qq.com <wanghonglei1>
2018-07-18 12:00:55 +0800

    转变接口调用封装类 api  apiservice
    Signed-off-by: 1667849141@qq.com <wanghonglei1>

--- android-table-lib/src/main/java/com/boco/table/view/CHScrollViewListener.java
+++
@@ -1,7 +0,0 @@
-package com.boco.table.view;
-
-public interface CHScrollViewListener {
-    //	public void onScrollChanged(int l, int t, int oldl, int oldt);
-    void onScrollXY(int offsetX, int offsetY);
-}
-
